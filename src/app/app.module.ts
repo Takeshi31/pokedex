bf1c537a4126411e6593df6bd9e78f47152f04d3
takeshi31 <heber87@gmail.com>
2022-11-24 22:02:58 -0500

    feat: Update routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokedexService } from './services/pokedex.service';
+import { HeaderComponent } from './modules/header/header.component';
+import { PokedexComponent } from './modules/pokedex/pokedex.component';
+import { PokedexModule } from './modules/pokedex/pokedex.module';
+import { ListPokemonsService } from './modules/pokedex/components/list-pokemons/list-pokemons.service';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PokedexComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PokedexModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [PokedexService, ListPokemonsService],
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
